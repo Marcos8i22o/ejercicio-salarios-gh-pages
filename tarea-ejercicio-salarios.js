107710b9c08c9416346a3e996ba7f9ad137d6498
Marcos8i22o <marcos.jinno@gmail.com>
2020-10-17 12:09:16 -0300

    estructura-principal

--- tarea-ejercicio-salarios.js
+++ tarea-ejercicio-salarios.js
@@ -1,7 +1,27 @@
 /*
 TAREA:
-Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
-Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
+Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) 
+inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
+Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, 
+menor salario anual, salario anual promedio y salario mensual promedio.
 
 Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
 */
+
+const $form = document.querySelector('#formulario-salarios-familiar');
+
+const $botonAgregarIntegrante = $form['#agregar-integrante'];
+const $botonQuitarIntegrante = $form['#quitar-integrante'];
+const $botonCalcular = $form['#calculos'];
+
+$botonAgregarIntegrante.onclick = function(){
+    return false;
+}
+
+$botonQuitarIntegrante.onclick = function(){
+    return false;
+}
+
+$botonCalcular.onclick = function(){
+    
+}
