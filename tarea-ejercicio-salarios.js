32aac5e4023fc018468da64d3e2458a22c98593f
Marcos8i22o <marcos.jinno@gmail.com>
2020-10-20 11:16:28 -0300

    Update tarea-ejercicio-salarios.js

--- tarea-ejercicio-salarios.js
+++ tarea-ejercicio-salarios.js
@@ -8,24 +8,38 @@ menor salario anual, salario anual promedio y salario mensual promedio.
 Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
 */
 
-const $botonAgregarIntegrante = document.querySelector('#agregar-integrante')
-const $botonQuitarIntegrante = document.querySelector('#quitar-integrante');
-const $botonCalcular = document.querySelector('#calculos');
+const $botonAgregarIntegrante = document.querySelector("#agregar-integrante");
+const $botonQuitarIntegrante = document.querySelector("#quitar-integrante");
+const $botonCalcular = document.querySelector("#calculos");
 
 const $integrantesTrabajadores = document.querySelector(
-    "#integrantes-trabajadores"
-  );
+  "#integrantes-trabajadores"
+);
 
-$botonAgregarIntegrante.onclick = function(){
-    agregarIntegrante($integrantesTrabajadores);
-    return false;
+function mostrarResultados(id, valor) {
+  document.querySelector(id).textContent += valor;
 }
 
-$botonQuitarIntegrante.onclick = function(){
-    quitarIntegrante($integrantesTrabajadores);
-    return false;
-}
+$botonAgregarIntegrante.onclick = function () {
+  agregarIntegrante($integrantesTrabajadores);
+  return false;
+};
 
-$botonCalcular.onclick = function(){
-    
-}
+$botonQuitarIntegrante.onclick = function () {
+  quitarIntegrante($integrantesTrabajadores);
+  return false;
+};
+
+$botonCalcular.onclick = function () {
+  const salariosAnuales = document.querySelectorAll(".salarios-anuales");
+  const resultados = [];
+
+  calcularSalarios(salariosAnuales, resultados);
+
+  document.querySelector("#salarios").className = "";
+
+  mostrarResultados("#mayor-salario-anual", resultados[0]);
+  mostrarResultados("#menor-salario-anual", resultados[1]);
+  mostrarResultados("#salario-anual-promedio", resultados[2]);
+  mostrarResultados("#salario-mensual-promedio", resultados[3]);
+};
