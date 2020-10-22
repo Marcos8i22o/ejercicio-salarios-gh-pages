3f1fb5ea13aeaab451c2c7d965d008d8cb47af36
Marcos8i22o <marcos.jinno@gmail.com>
2020-10-22 09:52:26 -0300

    se quitó un for
    para hacerlo todo dentro de un for solo.

--- calculos.js
+++ calculos.js
@@ -12,6 +12,7 @@ function calcularSalarios(salariosAnuales) {
 function obtenerMayorSalarioAnual(salariosAnuales) {
   let maximoSalario = 0;
 
+  //const salarios = Number(salariosAnuales.value);
   for (let i = 0; i < salariosAnuales.length; i++) {
     if (
       Number(salariosAnuales[i].value) != 0 &&
@@ -27,13 +28,12 @@ function obtenerMenorSalarioAnual(salariosAnuales) {
   let minimoSalario = Number(salariosAnuales[0].value);
 
   for (let i = 1; i < salariosAnuales.length; i++) {
-      if (
-        Number(salariosAnuales[i].value) != 0 &&
-        Number(salariosAnuales[i].value) < minimoSalario
-      ) {
-        minimoSalario = Number(salariosAnuales[i].value);
-      }
-    
+    if (
+      Number(salariosAnuales[i].value) != 0 &&
+      Number(salariosAnuales[i].value) < minimoSalario
+    ) {
+      minimoSalario = Number(salariosAnuales[i].value);
+    }
   }
   return minimoSalario;
 }
@@ -51,13 +51,10 @@ function obtenerSalarioAnualPromedio(salariosAnuales) {
 function obtenerSalarioMensualPromedio(salariosAnuales) {
   const MESES_DEL_ANIO = 12;
   const salarioMensual = [];
-  
+  let sumaSalariosMensuales = 0;
+
   for (let i = 0; i < salariosAnuales.length; i++) {
     salarioMensual[i] = Number(salariosAnuales[i].value) / MESES_DEL_ANIO;
-  }
-  
-  let sumaSalariosMensuales = 0;
-  for (let i = 0; i < salarioMensual.length; i++) {
     sumaSalariosMensuales += salarioMensual[i];
   }
 
